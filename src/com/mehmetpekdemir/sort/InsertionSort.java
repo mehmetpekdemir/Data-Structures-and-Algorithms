8029959e48754af13ce617a945e408d969bf455d
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-09-10 01:17:51 +0300

    Added insertion sort.

---
+++ src/com/mehmetpekdemir/sort/InsertionSort.java
@@ -0,0 +1,36 @@
+package com.mehmetpekdemir.sort;
+
+/**
+ * 
+ * <p>
+ * Worst Case Performance = O(n^2) - Quadratic
+ * </p>
+ * <p>
+ * Avarage Case Performance = O(n^2) - Quadratic
+ * </p>
+ * <p>
+ * Best Case Performance = O(n) - Linear
+ * </p>
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+public final class InsertionSort {
+
+	public static int[] sort(int[] array) {
+
+		for (int firstUnsortedIndex = 1; firstUnsortedIndex < array.length; firstUnsortedIndex++) {
+			int newElement = array[firstUnsortedIndex];
+
+			int i;
+
+			for (i = firstUnsortedIndex; i > 0 && array[i - 1] > newElement; i--) {
+				array[i] = array[i - 1];
+			}
+
+			array[i] = newElement;
+		}
+
+		return array;
+	}
+}
