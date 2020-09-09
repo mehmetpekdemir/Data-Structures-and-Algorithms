0b948326e14f83192b2215f246f545c4fc0dc129
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-09-10 01:18:21 +0300

    Added shell sort.

---
+++ src/com/mehmetpekdemir/sort/ShellSort.java
@@ -0,0 +1,42 @@
+package com.mehmetpekdemir.sort;
+
+/**
+ * 
+ * <p>
+ * Worst Case Performance = O(n^2) - Quadratic
+ * </p>
+ * <p>
+ * Avarage Case Performance = O(nlogn) - n log star n
+ * </p>
+ * <p>
+ * Best Case Performance = O(nlogn) - n log star n
+ * </p>
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+public final class ShellSort {
+
+	public static int[] sort(int[] array) {
+
+		for (int gap = array.length / 2; gap > 0; gap /= 2) {
+
+			for (int i = gap; i < array.length; i++) {
+				int newElement = array[i];
+
+				int j = i;
+
+				while (j >= gap && array[j - gap] > newElement) {
+					array[j] = array[j - gap];
+					j -= gap;
+				}
+
+				array[j] = newElement;
+			}
+
+		}
+
+		return array;
+	}
+
+}
