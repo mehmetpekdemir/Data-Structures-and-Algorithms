f22fdc6cade8be5ad31d2602a098883baa4e7075
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-09-09 19:43:29 +0300

    Added Selection Sort.

--- src/com/mehmetpekdemir/Main.java
+++ src/com/mehmetpekdemir/Main.java
@@ -1,6 +1,7 @@
 package com.mehmetpekdemir;
 
 import com.mehmetpekdemir.sort.BubbleSort;
+import com.mehmetpekdemir.sort.SelectionSort;
 
 /**
  * 
@@ -11,6 +12,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println("---------- Bubble Sort ----------");
 		// BubbleSort
 		int[] bubbleSortArray = { 20, 35, -15, 7, 55, 1, -22 };
 		bubbleSortArray = BubbleSort.sort(bubbleSortArray);
@@ -18,7 +20,14 @@ public class Main {
 			System.out.print(bubbleSortArray[i] + " ");
 		}
 
+		System.out.println("\n---------- Selection Sort ----------");
 		// SelectionSort
+
+		int[] selectionSort = { 20, 35, -15, 7, 55, 1, -22 };
+		selectionSort = SelectionSort.sort(selectionSort);
+		for (int i = 0; i < selectionSort.length; i++) {
+			System.out.print(selectionSort[i] + " ");
+		}
 
 	}
 
---
+++ src/com/mehmetpekdemir/sort/SelectionSort.java
@@ -0,0 +1,38 @@
+package com.mehmetpekdemir.sort;
+
+import com.mehmetpekdemir.shared.Util;
+
+/**
+ * <p>
+ * Worst Case Performance = O(n^2) - Quadratic
+ * </p>
+ * <p>
+ * Avarage Case Performance = O(n^2) - Quadratic
+ * </p>
+ * <p>
+ * Best Case Performance = O(n^2) - Quadratic
+ * </p>
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+public final class SelectionSort {
+
+	public static int[] sort(int[] array) {
+
+		for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+			int largest = 0;
+
+			for (int i = 1; i <= lastUnsortedIndex; i++) {
+				if (array[i] > array[largest]) {
+					largest = i;
+				}
+			}
+			Util.swap(array, largest, lastUnsortedIndex);
+		}
+
+		return array;
+	}
+
+}
