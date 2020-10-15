8bf6d6a6f3471c15590d13722f4bf8d0ef121820
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-10-15 21:27:53 +0300

    Singly Linked List added.

---
+++ src/com/mehmetpekdemir/list/singlylinkedlist/EmployeeLinkedList.java
@@ -0,0 +1,53 @@
+package com.mehmetpekdemir.list.singlylinkedlist;
+
+import com.mehmetpekdemir.list.Employee;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+public class EmployeeLinkedList {
+
+	private EmployeeNode head = null;
+	private int size = 0;
+
+	public void addToFront(Employee employee) {
+		EmployeeNode node = new EmployeeNode(employee);
+		node.setNext(head);
+		head = node;
+		size++;
+	}
+
+	public EmployeeNode removeFromFront() {
+		if (isEmpty()) {
+			return null;
+		}
+
+		EmployeeNode node = head;
+		head = head.getNext();
+		size--;
+		node.setNext(null);
+		return node;
+	}
+
+	public boolean isEmpty() {
+		return head == null;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public void printList() {
+		EmployeeNode current = head;
+		System.out.print("HEAD -> ");
+		while (current != null) {
+			System.out.print(current);
+			System.out.print(" -> ");
+			current = current.getNext();
+		}
+		System.out.println("null");
+	}
+
+}
