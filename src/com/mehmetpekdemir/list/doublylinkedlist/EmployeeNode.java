1553a29be15b44381295df8f481b1a2bfc034f45
Mehmet Pekdemir <mehmetpekdemir06@hotmail.com>
2020-10-16 01:10:47 +0300

    DoublyLinkedList added.

---
+++ src/com/mehmetpekdemir/list/doublylinkedlist/EmployeeNode.java
@@ -0,0 +1,49 @@
+package com.mehmetpekdemir.list.doublylinkedlist;
+
+import com.mehmetpekdemir.list.Employee;
+
+/**
+ * 
+ * @author MEHMET PEKDEMIR
+ * @since 1.0
+ */
+class EmployeeNode {
+
+	private Employee employee;
+	private EmployeeNode next;
+	private EmployeeNode previous;
+
+	public EmployeeNode(Employee employee) {
+		this.employee = employee;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public EmployeeNode getNext() {
+		return next;
+	}
+
+	public void setNext(EmployeeNode next) {
+		this.next = next;
+	}
+
+	public EmployeeNode getPrevious() {
+		return previous;
+	}
+
+	public void setPrevious(EmployeeNode previous) {
+		this.previous = previous;
+	}
+
+	@Override
+	public String toString() {
+		return employee.toString();
+	}
+	
+}
